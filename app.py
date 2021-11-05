b7a6516f895a742d293aeec4d7d4abbf45f8064b
fpavlica <frpavlica@gmail.com>
2021-11-05 01:52:05 +0000

    added a temporary way to access API through forms

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ db.create_database(con, con.cursor())
 def index():
     return r"<h1>Flask app :)</h1>"
 
-@app.route("/notes/add/<author>/<note>", methods = ["POST"])
-def notes_add(author, note):
-    # if (flask.request.method == 'POST'):
-        # author = flask.request.form('author')
-        # note = flask.request.form('note')
+@app.route("/notes/add/", methods = ["POST"])
+def notes_add():
+    if (flask.request.method == 'POST'):
+        author = flask.request.form('author')
+        note = flask.request.form('note')
 
     con = mysql_connect()
     db.add_note(con, con.cursor(), author, note)
@@ -33,19 +33,37 @@ def notes():
     all_notes = db.get_all_notes(con, con.cursor())
     return f"<p>{all_notes}</p>"
 
-@app.route("/notes/delete/<id>", methods = ["DELETE"])
-def notes_delete(id):
-    # id = flask.request.form('')
+@app.route("/notes/delete/", methods = ["DELETE"])
+def notes_delete():
+    id = flask.request.form('del_id')
     con = mysql_connect()
     num_del = db.delete_note_by_id(con, con.cursor(), id)
     return str(num_del), 200
-# @app.route("/sql/")
-# def sql():
-#     cnx = mysql.connector.connect(user='user', password='password',
-#                               host='127.0.0.1',
-#                               database='testdb')
-#     cnx.close()
-#     return "sql would be here"
+
+@app.route("notes_temp")
+def notes_temp():
+    return \
+        """<form action="/notes/add" method = "POST">
+                <label for="author">Your name: </label>
+                <input type="text" id="author" name="author">
+                <label for="note">Your note: </label>
+                <input type="text" id="note" name="note"> 
+                <input type="submit" value="Add note" />
+
+            </form>
+
+            <form action="/notes">
+                <input type="submit" value="View notes" />
+            </form>
+
+            <form action="/notes/delete method="DELETE">
+                <label for="del_id"> ID of note to delete: </label>
+                <input type="text" id="del_id" name="del_id">
+                <input type="submit" value="Delete note" />
+            </form>
+
+        """
+    pass
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
